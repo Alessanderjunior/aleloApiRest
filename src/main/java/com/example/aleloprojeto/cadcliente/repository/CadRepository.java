ac37094550eea181e368cc5b73d8eabb46db7e45
Alessanderjunior <alessanderjr.dev@gmail.com>
2020-12-04 20:04:59 -0300

    Update tests and missing statements

--- src/main/java/com/example/aleloprojeto/cadcliente/repository/CadRepository.java
+++ src/main/java/com/example/aleloprojeto/cadcliente/repository/CadRepository.java
@@ -1,8 +1,11 @@
 package com.example.aleloprojeto.cadcliente.repository;
 import com.example.aleloprojeto.cadcliente.entity.Companies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //classe de iniciação de repositório
 
+
+@Repository
 public interface CadRepository extends JpaRepository<Companies, Long>{
 }
--- src/main/java/com/example/aleloprojeto/cadcliente/service/CadService.java
+++ src/main/java/com/example/aleloprojeto/cadcliente/service/CadService.java
@@ -6,13 +6,13 @@ import com.example.aleloprojeto.cadcliente.entity.Companies;
 import com.example.aleloprojeto.cadcliente.repository.CadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Service
 public class CadService {
 
-    private CadRepository cadRepository;
+    private final CadRepository cadRepository;
 
     //classe responsável pela criação de empresas
 
@@ -22,7 +22,7 @@ public class CadService {
         this.cadRepository = cadRepository;
     }
 
-    @PostMapping
+
     public MessageResponseDTO create( Companies company){
         Companies savedCompany = cadRepository.save(company);
         return MessageResponseDTO.builder()
