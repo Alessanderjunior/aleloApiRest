cf9b12bde1cf2b16ee0892bb2ed7421aba1b7ea0
Alessanderjunior <alessanderjr.dev@gmail.com>
2020-12-04 11:57:12 -0300

    First Commit

---
+++ src/main/java/com/example/aleloprojeto/cadcliente/CadclienteApplication.java
@@ -0,0 +1,13 @@
+package com.example.aleloprojeto.cadcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aleloprojeto/cadcliente/CadclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aleloprojeto.cadcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
